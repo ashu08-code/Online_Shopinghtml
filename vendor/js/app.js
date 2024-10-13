798c07162e43b4d11878ed2c901a6867ebbf7856
ashu08-code <162013252+ashu08-code@users.noreply.github.com>
2024-10-13 20:50:23 +0530

    Update app.js

--- vendor/js/app.js
+++ vendor/js/app.js
@@ -1,7 +1,7 @@
  // ---- top timer ----
  function makeTimer() {
 
-    var endTime = new Date("22 March 2024 10:56:00 GMT+01:00");
+    var endTime = new Date("22 March 2025 10:56:00 GMT+01:00");
     endTime = (Date.parse(endTime) / 1000);
 
     var now = new Date();
